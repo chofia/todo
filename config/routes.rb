f38923d9ab299d211f5c943332ca117882201944
chofia <sofia.saenz@ymail.com>
2018-05-14 19:59:23 -0400

    todo items and lists working

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
-  resources :todo_lists
-
   root "todo_lists#index"
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  resources :todo_lists do
+    resources :todo_items, only: [:create, :destroy]
+  end
 end
+
+
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
