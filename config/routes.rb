22312044fc856feaf727327c348010d1f351b56b
chofia <sofia.saenz@ymail.com>
2018-05-14 20:25:39 -0400

    Complete method is working

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,11 @@ Rails.application.routes.draw do
   root "todo_lists#index"
 
   resources :todo_lists do
-    resources :todo_items, only: [:create, :destroy]
+    resources :todo_items, only: [:create, :destroy] do
+      member do
+        patch :complete
+      end
+    end
   end
 end
 
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180514225154) do
+ActiveRecord::Schema.define(version: 20180515000239) do
 
   create_table "todo_items", force: :cascade do |t|
     t.string "content"
@@ -18,6 +18,7 @@ ActiveRecord::Schema.define(version: 20180514225154) do
     t.integer "todo_list_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.datetime "completed_at"
     t.index ["todo_list_id"], name: "index_todo_items_on_todo_list_id"
   end
 
